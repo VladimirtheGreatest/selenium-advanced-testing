44dc00cd474a074f147890de95f63545ea6d12e7
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-08-08 14:47:30 +0100

    Update story2.js
    . As a user I want to select the ‘Summer Dresses’ option from the navigation menu, so
    that I can view an item from the summer collection.
    Acceptance Criteria:
    • On mouse-hover button ‘WOMAN’, sub navigation options will appear
    • Summer items only display inside the search result

--- tests/story2.js
+++ tests/story2.js
@@ -2,7 +2,7 @@ require('chromedriver');
 const {Browser, By, Key, until} = require("selenium-webdriver");
 const {suite} = require("selenium-webdriver/testing");
 const assert = require('assert');
-const SummerDress = require('../pages/page2.js')
+const ShoppingCart = require('../pages/page1.js')
 
 
 suite(function(env) {
@@ -14,17 +14,22 @@ suite(function(env) {
 
         before(async function() {
             driver = await env.builder().build();
-            page = new SummerDress(driver);
+            page = new ShoppingCart(driver);
             await page.open();
         });
 
 
                 it('On mouse-hover button ‘WOMAN’, sub navigation options will appear', async function() {
-
+                  await page.MouseOver();
+                  let subNavigation = await driver.findElement(page.locators.SubNav);
+                  assert(subNavigation.isDisplayed());
                 });
 
                 it('Summer items only display inside the search results', async function() {
-
+                  await driver.findElement(page.locators.SummerDressesLink).click();
+                  let searchResults = await driver.findElement(page.locators.SummerMenu);
+                  let searchResults2 = await driver.findElement(page.locators.SummerMenu2);
+                  assert(searchResults && searchResults2.isDisplayed());
                 });
 
 
