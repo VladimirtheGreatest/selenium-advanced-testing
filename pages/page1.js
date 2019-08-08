9f098b7b4bae416f7571aa11ee3b5caa0f3984c2
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-08-08 14:46:40 +0100

    Update page1.js

--- pages/page1.js
+++ pages/page1.js
@@ -1,7 +1,6 @@
 require('chromedriver');
 const {Browser, By, Key, until} = require("selenium-webdriver");
 const url = "http://automationpractice.com/index.php";
-const url2 = "http://automationpractice.com/index.php?controller=order"
 
 class ShoppingCart {
   constructor(driver){
@@ -11,7 +10,13 @@ class ShoppingCart {
       basketMessage : By.css('#center_column > p'),
       addToCart : By.xpath('//*[@id="homefeatured"]/li[1]/div/div[2]/div[2]/a[1]'),
       Checkout : By.xpath('//*[@id="layer_cart"]/div[1]/div[2]/div[4]/a'),
-      BasketItems : By.xpath('//*[@id="summary_products_quantity"]')
+      BasketItems : By.xpath('//*[@id="summary_products_quantity"]'),
+      WomanNav: By.xpath('//*[@id="block_top_menu"]/ul/li[1]/a'),
+      SubNav: By.xpath('//*[@id="block_top_menu"]/ul/li[1]/ul'),
+      SummerDressesLink: By.xpath('//*[@id="block_top_menu"]/ul/li[1]/ul/li[2]/ul/li[3]/a'),
+      SummerMenu: By.css('#center_column > h1 > span.cat-name'),
+      SummerMenu2: By.css('#columns > div.breadcrumb.clearfix > span:nth-child(6)')
+
     }
   }
 
@@ -28,6 +33,13 @@ class ShoppingCart {
   }
   async DeleteItem() {
       this.driver.findElement(this.locators.deleteButton).click();
+      await this.driver.sleep(2000);
+  }
+  async MouseOver() {
+      let womanNav = await this.driver.findElement(this.locators.WomanNav);
+      const actions = await this.driver.actions({bridge: true});
+      //await this.driver.actions.moveToElement(womanNav).perform();  this function doesnt work on my pc for mysterious reasons :(
+      await actions.move({duration:2000,origin:womanNav,x:0,y:0}).perform();
       await this.driver.sleep(2000);
   }
 
